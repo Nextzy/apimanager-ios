0b6d5db165cfcfd4662fda86024c08937cb75b80
Thongpak Pongsilathong <thongpak21@gmail.com>
2017-09-18 09:30:35 +0700

    init project

---
+++ APIManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APIManager
+//
+//  Created by Thongpak on 9/18/2560 BE.
+//  Copyright © 2560 Thongpak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
